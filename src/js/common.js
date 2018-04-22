ebe792e40e1f7cd25d386da8061af569680b5973
DimkaSklyar <DimkaSklyar@mail.ru>
2018-04-23 01:27:38 +0300

    add carousel and animation

--- src/js/common.js
+++ src/js/common.js
@@ -25,7 +25,7 @@ $(function() {
 
 
 //carousel
-$('.owl-carousel').owlCarousel({
+$('.services-items').owlCarousel({
 		loop: true,
     margin:10,
 		dots: true,
@@ -40,6 +40,99 @@ $('.owl-carousel').owlCarousel({
             items:3
         }
     }
-})
+});
+
+var owl = $('.team-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+	dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+
+$('.team-next').click(function() {
+    owl.trigger('next.owl.carousel');
+});
+
+$('.team-prev').click(function() {
+    owl.trigger('prev.owl.carousel');
+});
+
+
+var owl2 = $('.partners-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+	dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+
+$('.partners-next').click(function() {
+    owl2.trigger('next.owl.carousel');
+});
+
+$('.partners-prev').click(function() {
+    owl2.trigger('prev.owl.carousel');
+});
+
+var owl3 = $('.blog-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+	dots: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:2
+        }
+    }
+});
+
+$('.blog-next').click(function() {
+    owl3.trigger('next.owl.carousel');
+});
+
+$('.blog-prev').click(function() {
+    owl3.trigger('prev.owl.carousel');
+});
+
+$('.shop-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+	dots: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+});
 
 });
