326a1b59546e4550a2c2389626aa1e1e9a54f7af
Dimka <dimkasklyar@mail.ru>
2018-04-26 10:32:54 +0300

    v.1.0.1
    add method POST for form

---
+++ src/question.php
@@ -0,0 +1,25 @@
+<?php 
+if($_POST) 
+{ 
+$to = "sabotender@mail.ru"; //куда отправлять письмо 
+$subject = "ВОПРОС!"; //тема 
+$message = '<span style="font-weight:bold;color:#ff6600;font-size:18px;"><i>ВОПРОС!</i> </span><br><br> 
+Имя: <span style="font-weight:bold;color:#000;">'.$_POST['name'].'</span><br> 
+E-mail: <span style="font-weight:bold;color:#339900;">'.$_POST['email'].'</span><br> 
+Вопрос: <span style="font-weight:bold;color:#339900;font-size:24px;"> '.$_POST['question'].'</span>'; 
+$headers = "Content-type: text/html; charset=UTF-8 \r\n"; 
+$headers .= "From: <info@авто-трансфер-юг.рф\r\n"; // от кого, придумайте ящик или укажите почту своего сайта. 
+$result = mail($to, $subject, $message, $headers); 
+
+if ($result){ 
+echo "<p style='text-transform: uppercase; 
+font-size: 1.4em; 
+background-color: #fecb18; 
+font-weight: 700; 
+padding: .4em; 
+box-shadow: 0 19px 49px 0px rgba(81,74,48,0.3); 
+text-align: center;'>Сообщение успешно отправлено.</br> 
+Скоро Вам перезвонят</p>"; 
+} 
+} 
+?>
